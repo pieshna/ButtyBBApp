68143e787bdc4f4d2a0b02a88f0401cbc46752f8
Jose Cabrera <cabreraj525@gmail.com>
2023-10-24 15:56:05 -0600

    actualizamos template para que se muestre correctamente

--- src/Template.tsx
+++ src/Template.tsx
@@ -5,18 +5,21 @@ import { token } from './tools/constantes'
 function Template() {
   return (
     <div>
-      {token() && <NavBar />}
-      <div className="h-screen">
-        <Rutas />
-      </div>
-      {token() && (
-        <div className="h-10 bg-pateleta-600">
-          <p className="text-center text-white pt-2">
-            © 2023 - ButtyBB - Todos los derechos reservados. - Desarrollado por
-            Randi Islau Villeda Escobar
-          </p>
+      <div className="h-screen flex flex-col">
+        {token() && <NavBar />}
+        <div className="flex-1 overflow-y-auto">
+          <Rutas />
         </div>
-      )}
+
+        {token() && (
+          <div className="h-10 bg-pateleta-600">
+            <p className="text-center text-white pt-2">
+              © 2023 - ButtyBB - Todos los derechos reservados. - Desarrollado
+              por Randi Islau Villeda Escobar
+            </p>
+          </div>
+        )}
+      </div>
     </div>
   )
 }
