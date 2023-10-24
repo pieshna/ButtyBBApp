09976306b8b8e59bb7a41d8147c29dcd6bc7417d
Jose Cabrera <cabreraj525@gmail.com>
2023-10-23 20:48:36 -0600

    agregamos footer

--- src/Template.tsx
+++ src/Template.tsx
@@ -6,7 +6,17 @@ function Template() {
   return (
     <div>
       {token() && <NavBar />}
-      <Rutas />
+      <div className="h-screen">
+        <Rutas />
+      </div>
+      {token() && (
+        <div className="h-10 bg-pateleta-600">
+          <p className="text-center text-white pt-2">
+            © 2023 - ButtyBB - Todos los derechos reservados. - Desarrollado por
+            Randi Islau Villeda Escobar
+          </p>
+        </div>
+      )}
     </div>
   )
 }
