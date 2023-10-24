665ad84d2fbe44ac31ef4753e2ba6fdfe3d9aaf0
Jose Cabrera <cabreraj525@gmail.com>
2023-10-23 20:48:29 -0600

    mostramos navbar segun rol

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,40 +6,84 @@ import { Link } from 'react-router-dom'
 import { decodeToken } from '../tools/constantes'
 
 function NavBar() {
-  const { usuario } = decodeToken()
-  return (
-    <Navbar expand="lg" className="bg-pateleta-300">
-      <Container>
-        <Link to="/">
-          <Navbar.Brand href="/">ButtyBB</Navbar.Brand>
-        </Link>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto">
-            <Link to="/clientes">
-              <Nav.Link href="/clientes">Clientes</Nav.Link>
-            </Link>
-            <Link to="/proveedores">
-              <Nav.Link href="/proveedores">Proveedores</Nav.Link>
-            </Link>
-            <Link to="/usuarios">
-              <Nav.Link href="/usuarios">Usuarios</Nav.Link>
-            </Link>
-            <Link to="/productos">
-              <Nav.Link href="/productos">Productos</Nav.Link>
-            </Link>
-            <NavDropdown title={usuario} id="basic-nav-dropdown">
-              <Link to="/logout">
-                <NavDropdown.Item href="/logout">
-                  Cerrar Sesion.
-                </NavDropdown.Item>
+  const { usuario, rol } = decodeToken()
+
+  const returnAdmin = () => {
+    return (
+      <Navbar expand="lg" className="bg-pateleta-300">
+        <Container>
+          <Link to="/">
+            <Navbar.Brand href="/">ButtyBB</Navbar.Brand>
+          </Link>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Link to="/clientes">
+                <Nav.Link href="/clientes">Clientes</Nav.Link>
               </Link>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  )
+              <Link to="/proveedores">
+                <Nav.Link href="/proveedores">Proveedores</Nav.Link>
+              </Link>
+              <Link to="/productos">
+                <Nav.Link href="/productos">Productos</Nav.Link>
+              </Link>
+              <NavDropdown title="Administracion" id="basico-nav-dropdown">
+                <Link to="/usuarios">
+                  <NavDropdown.Item href="/usuarios">Usuarios</NavDropdown.Item>
+                </Link>
+                <Link to="/compras">
+                  <NavDropdown.Item href="/compras">Compras</NavDropdown.Item>
+                </Link>
+                <Link to="/ventas">
+                  <NavDropdown.Item href="/ventas">Ventas</NavDropdown.Item>
+                </Link>
+              </NavDropdown>
+              <NavDropdown title={usuario} id="basic-nav-dropdown">
+                <Link to="/logout">
+                  <NavDropdown.Item href="/logout">
+                    Cerrar Sesion.
+                  </NavDropdown.Item>
+                </Link>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    )
+  }
+
+  const returnUser = () => {
+    return (
+      <Navbar expand="lg" className="bg-pateleta-300">
+        <Container>
+          <Link to="/">
+            <Navbar.Brand href="/">ButtyBB</Navbar.Brand>
+          </Link>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Link to="/clientes">
+                <Nav.Link href="/clientes">Clientes</Nav.Link>
+              </Link>
+              <Link to="/ventas">
+                <Nav.Link href="/ventas">Ventas</Nav.Link>
+              </Link>
+
+              <NavDropdown title={usuario} id="basic-nav-dropdown">
+                <Link to="/logout">
+                  <NavDropdown.Item href="/logout">
+                    Cerrar Sesion.
+                  </NavDropdown.Item>
+                </Link>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    )
+  }
+
+  return rol === 'Administrador' ? returnAdmin() : returnUser()
 }
 
 export default NavBar
