2d2c05f1b3a37a55dbd7f2dd5d4c352f30a4364b
Jose Cabrera <cabreraj525@gmail.com>
2023-11-06 04:05:05 -0600

    corregimos error de pagina en blanco si esta iniciada sesion

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { decodeToken } from './tools/constantes'
 function App() {
   const navigate = useNavigate()
 
   useEffect(() => {
     if (!sessionStorage.getItem('token')) {
       navigate('/login')
+    } else {
+      decodeToken().usuarioId ? navigate('/home') : navigate('/login')
     }
   })
   return <></>
