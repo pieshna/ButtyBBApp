f6341fe259c1808f048446908ac2cf92c077ac0c
Jose Cabrera <cabreraj525@gmail.com>
2023-11-06 03:59:34 -0600

    hacemos lazy load

--- src/Rutas.tsx
+++ src/Rutas.tsx
@@ -1,43 +1,50 @@
+import { Suspense, lazy } from 'react'
 import { Route, Routes } from 'react-router-dom'
-import Home from './Home'
-import App from './App'
-import Login from './auth/Login'
-import Logout from './auth/Logout'
-import VerProveedor from './proveedor/VerProveedor'
-import VerUsuario from './usuarios/VerUsuario'
-import VerCliente from './clientes/VerCliente'
-import VerProducto from './productos/VerProducto'
-import Compras from './compraventa/Compras'
 import { decodeToken } from './tools/constantes'
-import Ventas from './compraventa/Ventas'
+
+const Home = lazy(() => import('./Home'))
+const App = lazy(() => import('./App'))
+const Login = lazy(() => import('./auth/Login'))
+const Logout = lazy(() => import('./auth/Logout'))
+const VerProveedor = lazy(() => import('./proveedor/VerProveedor'))
+const VerUsuario = lazy(() => import('./usuarios/VerUsuario'))
+const VerCliente = lazy(() => import('./clientes/VerCliente'))
+const VerProducto = lazy(() => import('./productos/VerProducto'))
+const Compras = lazy(() => import('./compraventa/Compras'))
+const Ventas = lazy(() => import('./compraventa/Ventas'))
+
 function Rutas() {
   const rol = decodeToken()?.rol || ''
   const esAdmin = () => {
     return (
-      <Routes>
-        <Route path="/ventas" element={<Ventas />} />
-        <Route path="/compras" element={<Compras />} />
-        <Route path="/productos" element={<VerProducto />} />
-        <Route path="/clientes" element={<VerCliente />} />
-        <Route path="/usuarios" element={<VerUsuario />} />
-        <Route path="/proveedores" element={<VerProveedor />} />
-        <Route path="/logout" element={<Logout />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/" element={<App />} />
-        <Route path="/home" element={<Home />} />
-      </Routes>
+      <Suspense fallback={<div></div>}>
+        <Routes>
+          <Route path="/ventas" element={<Ventas />} />
+          <Route path="/compras" element={<Compras />} />
+          <Route path="/productos" element={<VerProducto />} />
+          <Route path="/clientes" element={<VerCliente />} />
+          <Route path="/usuarios" element={<VerUsuario />} />
+          <Route path="/proveedores" element={<VerProveedor />} />
+          <Route path="/logout" element={<Logout />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<App />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </Suspense>
     )
   }
   const esEmpleado = () => {
     return (
-      <Routes>
-        <Route path="/ventas" element={<Ventas />} />
-        <Route path="/clientes" element={<VerCliente />} />
-        <Route path="/logout" element={<Logout />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/" element={<App />} />
-      </Routes>
+      <Suspense fallback={<div></div>}>
+        <Routes>
+          <Route path="/ventas" element={<Ventas />} />
+          <Route path="/clientes" element={<VerCliente />} />
+          <Route path="/logout" element={<Logout />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<App />} />
+        </Routes>
+      </Suspense>
     )
   }
   return rol === 'Administrador' ? esAdmin() : esEmpleado()
